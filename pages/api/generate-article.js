78e17143f5bb695c7a85b0183acf6720021a9fa3
Maryen <voujum@gmail.com>
2023-05-25 22:25:50 +0200

    replaced model text-davinci-003 with gpt-3.5-turbo

--- pages/api/generate-article.js
+++ pages/api/generate-article.js
@@ -8,24 +8,53 @@ export default async function handler(req, res) {
   });
 
   const openai = new OpenAIApi(config);
-  const response = await openai.createCompletion({
-    model: "text-davinci-003",
+
+  const response = await openai.createChatCompletion({
+    model: "gpt-3.5-turbo",
     temperature: 0,
-    max_tokens: 2600,
-    prompt: `Write short, well structured and detailed SEO-friendly article about "${subject}", that targets the following comma-separated keywords: ${keywords}. 
-    The content should be formatted in SEO friendly HTML. 
-    The response must also include appropriate HTML title and meta description content.
-    The return format must strictly be a stringified JSON (and nothing else) in the following format: 
-    {
-      "articleContent": article content here,
-      "title": title goes here,
-      "metaDescription: meta description goes here
-    }`,
+    messages: [
+      {
+        role: "system",
+        content: "You are an article generator",
+      },
+      {
+        role: "user",
+        content: `Write short, well structured and detailed SEO-friendly article about "${subject}", that targets the following comma-separated keywords: ${keywords}.
+        The content should be formatted in SEO friendly HTML.
+        The response must also include appropriate HTML title and meta description content.
+        The return format must strictly be a stringified JSON (and nothing else) in the following format: 
+        {
+          "articleContent": article content here,
+          "title": title goes here,
+          "metaDescription: meta description goes here
+        }`,
+      },
+    ],
   });
 
-  console.log("response", response.data.choices[0].text.split("\n").join(""));
+  console.log("response: ", response.data.choices[0].message.content);
 
   res.status(200).json({
-    article: JSON.parse(response.data.choices[0].text.split("\n").join("")),
+    article: JSON.parse(response.data.choices[0].message.content),
   });
+
+  // const response = await openai.createCompletion({
+  //   model: "text-davinci-003",
+  //   temperature: 0,
+  //   max_tokens: 2600,
+  //   prompt: `Write a short, well structured and detailed SEO-friendly
+  //   article about "${subject}", that targets the following comma-separated keywords: ${keywords}.
+  //   The content should be formatted in SEO friendly HTML.
+  //   The response must also include appropriate HTML title and meta description content.
+  //   The return format must strictly be a stringified JSON (and nothing else) in the following format:
+  //   {
+  //     "articleContent": article content here,
+  //     "title": title goes here,
+  //     "metaDescription: meta description goes here
+  //   }`,
+  // });
+
+  // res.status(200).json({
+  //   article: JSON.parse(response.data.choices[0].text.split("\n").join("")),
+  // });
 }
