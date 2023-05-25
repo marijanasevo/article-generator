cd1caa046f306132174a12c5d93a8af14f6d6511
Maryen <voujum@gmail.com>
2023-05-25 20:21:43 +0200

    UI form to customize prompt

--- pages/api/generate-article.js
+++ pages/api/generate-article.js
@@ -1,8 +1,7 @@
 import { Configuration, OpenAIApi } from "openai";
 
 export default async function handler(req, res) {
-  const subject = "How to have bond with your cat";
-  const keywords = "how to take care of a cat, benefits of having a cat";
+  const { subject, keywords } = req.body;
 
   const config = new Configuration({
     apiKey: process.env.OPENAI_API_KEY,
@@ -12,14 +11,14 @@ export default async function handler(req, res) {
   const response = await openai.createCompletion({
     model: "text-davinci-003",
     temperature: 0,
-    max_tokens: 1000,
-    prompt: `Write a short, well structured and detailed SEO-friendly article about "${subject}", that targets the following comma-separated keywords: ${keywords}. 
+    max_tokens: 2600,
+    prompt: `Write short, well structured and detailed SEO-friendly article about "${subject}", that targets the following comma-separated keywords: ${keywords}. 
     The content should be formatted in SEO friendly HTML. 
     The response must also include appropriate HTML title and meta description content.
-    The return format must be stringified JSON in the following format: 
+    The return format must strictly be a stringified JSON (and nothing else) in the following format: 
     {
-      "articleContent": article content here
-      "title": title goes here
+      "articleContent": article content here,
+      "title": title goes here,
       "metaDescription: meta description goes here
     }`,
   });
--- pages/article/new.js
+++ pages/article/new.js
@@ -4,10 +4,18 @@ import { useState } from "react";
 
 export default function NewArticle() {
   const [articleContent, setArticleContent] = useState("");
+  const [subject, setSubject] = useState("");
+  const [keywords, setKeywords] = useState("");
 
-  const handleClick = async () => {
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
     const response = await fetch("/api/generate-article", {
       method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ subject, keywords }),
     });
 
     const json = await response.json();
@@ -17,11 +25,31 @@ export default function NewArticle() {
 
   return (
     <div>
-      <h1>This is the new article page</h1>
-      <p>and</p>
-      <button className="button" onClick={handleClick}>
-        Generate
-      </button>
+      <form onSubmit={handleSubmit} action="">
+        <div>
+          <label>
+            <strong>I need an article on the subject of:</strong>
+            <textarea
+              className="textarea-input"
+              value={subject}
+              onChange={(e) => setSubject(e.target.value)}
+            />
+          </label>
+        </div>
+        <div>
+          <label>
+            <strong>That targets the following keywords:</strong>
+            <textarea
+              className="textarea-input"
+              value={keywords}
+              onChange={(e) => setKeywords(e.target.value)}
+            />
+          </label>
+        </div>
+        <button type="submit" className="button">
+          Generate
+        </button>
+      </form>
       <div
         className="max-w-screen-sm p-10 "
         dangerouslySetInnerHTML={{ __html: articleContent }}
